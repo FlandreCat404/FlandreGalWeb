68d7c330c1171f7c138c33f6faec7d1dc16d423c
FlandreCat404 <781424247@qq.com>
2022-10-19 11:17:17 +0800

    first commit

---
+++ FlandreGalWeb/src/test/java/com/FlandreGalWeb/FlandreGalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlandreGalWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlandreGalWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
